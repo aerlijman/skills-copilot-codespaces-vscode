d8e7609941ce87b4281b639f4a1c6f8d79fcdc43
Ariel Erlijman <erlijman@gmail.com>
2023-06-25 19:54:20 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    return {
+        name: 'skillsMember',
+        templateUrl: 'app/components/skills/member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'skillsMemberCtrl'
+    }
+}
